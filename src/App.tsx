6c61aa9c72c8ae11dd5cdf152086ba2ddf1341f1
brunoviana <bruno.viana@fast2mine.com>
2023-11-07 16:23:41 -0300

    função de primeiro grau funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import { funcaoDePrimeiroGrau, funcaoDeSegundoGrau } from './utils/parser';
 import './App.css';
 
+
 function App() {
+  const [input, setInput] = useState("");
+  // Button Text
+  const [resultado, setResultado] = useState<any | null>(null);
+
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    try {
+      event.preventDefault();
+      const test = funcaoDePrimeiroGrau(input);
+      //TODO testar se é primeiro grau ou segundo usando a tokenização
+      // funcaoDeSegundoGrau
+      setResultado("Resultado: " + test);    
+    } catch (error) {
+      setResultado("Não foi possível fazer a análise dos valores inseridos, tente no formato: f(3)=4x*2")
+    }
+  
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><h1>LF e Compiladores</h1>
+    <div className='container'>
+      <form onSubmit={handleSubmit} id='myForm'>
+        <label>
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)} />
+        </label>
+        <button type="submit"> Enviar </button>
+      </form>
     </div>
+    
+    <div id="result">
+      <h1 id='resultado'>{resultado}</h1></div></>
+    
+    
   );
 }
 
